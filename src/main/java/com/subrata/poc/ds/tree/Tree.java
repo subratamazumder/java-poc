a4d63f56e3a0ad513d662ab933d145506ce38752
Subrata Mazumder <subrata.besu@gmail.com>
2020-09-09 04:30:50 +0530

    structure added

--- src/main/java/com/subrata/poc/ds/tree/Tree.java
+++ src/main/java/com/subrata/poc/ds/tree/Tree.java
@@ -1,17 +1,35 @@
 package com.subrata.poc.ds.tree;
+import org.apache.commons.lang3.time.StopWatch;
+
+import static com.subrata.poc.util.MyUtil.*;
 
 /**
- * Hello world!
+ * Template Class to work with various scenarios
  */
 public final class Tree {
+    private static StopWatch watch = new StopWatch();
     private Tree() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello Tree!");
+        printJavaVersion();
+        pocScenarioA();
+        pocScenarioB();
+    }
+    private static void pocScenarioA(){
+        logCurrentPosition(Thread.currentThread().getStackTrace());
+        logTimeStart(watch);
+        //Write your code here - start
+
+        //Write your code here - finish
+        logTimeFinish(watch);
+    }
+    private static void pocScenarioB(){
+        logCurrentPosition(Thread.currentThread().getStackTrace());
+        logTimeStart(watch);
+        //Write your code here - start
+
+        //Write your code here - finish
+        logTimeFinish(watch);
     }
 }
